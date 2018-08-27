e2c0db198efe103ade513548db5fcf8adc21d030
Oscar Garcia <oscar.garcia@grupperalada.com>
2018-08-27 13:21:37 +0200

    composer ampliado

--- deploy.php
+++ deploy.php
@@ -9,26 +9,38 @@ set('ssh_type', 'native');
 set('ssh_multiplexing', true);
 set('keep_releases', 5);
 
-set('repository', '');
+set('repository', 'https://' . $user . ':' . $pass . '@bitbucket.org/peraladaweb/finca_mas_solers_web.git');
 
 add('shared_files', ['web/.htaccess']);
-add('shared_dirs', []);
+add('shared_dirs', ['web/uploads']);
 
-add('writable_dirs', []);
+add('writable_dirs', ['web/uploads']);
+set('writable_mode', 'chmod');
+set('writable_chmod_mode', '775');
 
-// Servers
+// Serversaa
 serverList('servers.yml');
 
 
 // Tasks
 
-desc('Restart PHP-FPM service');
-task('php-fpm:restart', function () {
-    // The user must have rights for restart service
-    // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
-    run('sudo systemctl restart php-fpm.service');
-});
-after('deploy:symlink', 'php-fpm:restart');
+//desc('Restart PHP-FPM service');
+//task('php-fpm:restart', function () {
+//    // The user must have rights for restart service
+//    // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
+//    run('sudo systemctl restart php-fpm.service');
+//});
+//after('deploy:symlink', 'php-fpm:restart');
+
+/**
+ * install FOS ckeditor
+ */
+task('fosckeditor:install', function () {
+	run('{{env_vars}} {{bin/php}} {{bin/console}} ckeditor:install');
+	run('{{env_vars}} {{bin/php}} {{bin/console}} assets:install web');
+})->desc('Migrate database');
+
+after('deploy:assets:install', 'fosckeditor:install');
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
